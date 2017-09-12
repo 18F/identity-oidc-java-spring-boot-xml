55a76a4bbc42f6412a5ca3789e6d7c7e07a9d2c4
Justin Richer <justin@bspk.io>
2017-09-12 16:12:20 -0400

    added XML-based configuration

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,8 +2,10 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:servlet-context.xml")
 public class Application {
 
     public static void main(String[] args) throws Throwable {
